260e126e59a8ded4e2ebc3cac6aac805b8532518
jain17akshat <jain.7akshat@gmail.com>
2025-07-28 11:18:36 +0530

    Initial commit

---
+++ scripts/login.js
@@ -0,0 +1,19 @@
+
+    function login(e) {
+      e.preventDefault();
+
+      const email = document.getElementById("loginEmail").value;
+      const password = document.getElementById("loginPassword").value;
+
+      const savedEmail = localStorage.getItem("fb_user_email");
+      const savedPassword = localStorage.getItem("fb_user_password");
+
+      if (email === savedEmail && password === savedPassword) {
+        alert("Login successful!");
+        window.location.href = "index.html"; 
+      } else {
+        alert("Invalid email or password!");
+      }
+
+      return false;
+    }
---
+++ scripts/signup.js
@@ -0,0 +1,21 @@
+
+function createAccount(e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirm = document.getElementById("confirm").value;
+
+  if (password !== confirm) {
+    alert("Passwords do not match!");
+    return false;
+  }
+
+  localStorage.setItem("fb_user_email", email);
+  localStorage.setItem("fb_user_password", password);
+  localStorage.setItem("fb_user_name", name);
+
+  alert("Account created! Please log in.");
+  window.location.href = "login.html"; // Redirect to login
+  return false;
+}
